050d497d09580cb8cf9fb14dbed0ec8b08b2024f
leonitacecilia <leonitacecilia19@gmail.com>
2024-09-07 15:25:20 +0700

    Adding background to website

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'nama_aplikasi' : 'Toko Pacilians',
-        'nama_pembuat': 'Leonita',
+        'nama_pembuat': 'Leonita Cecilia',
         'kelas': 'PBP A'
     }
 
